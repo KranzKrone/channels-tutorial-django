f9b23f0f0eebaa4caf6c5c8c915384580bd213d1
kranzkrone <dev@st-page.de>
2018-09-18 14:42:23 +0200

    Aktueller Stand

--- mysite/chat/routing.py
+++ mysite/chat/routing.py
@@ -1,8 +1,8 @@
 # chat/routing.py
 from django.conf.urls import url
 
-from . import consumers
+from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    url(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer),
 ]
